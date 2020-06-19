c71eb796cd4eacf5bbdeccb8af29032b4a26e7a5
Trevor Ryan Thornton <trthorn2@ncsu.edu>
2020-06-19 18:36:25 -0400

    remove custom again

--- db/schema.rb
+++ db/schema.rb
@@ -13,48 +13,49 @@
 ActiveRecord::Schema.define(version: 2018_09_18_141027) do
 
   create_table "agent_associations", force: :cascade do |t|
-    t.integer "record_id", limit: 4
-    t.string "record_type", limit: 255
-    t.integer "agent_id", limit: 4
-    t.string "role", limit: 255
-    t.string "function", limit: 255
-    t.string "relator", limit: 255
-    t.integer "position", limit: 4
+    t.string "record_type"
+    t.integer "record_id"
+    t.integer "agent_id"
+    t.string "role"
+    t.string "function"
+    t.string "relator"
+    t.integer "position"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text "terms", limit: 4294967295
+    t.text "terms"
     t.index ["agent_id"], name: "index_agent_associations_on_agent_id"
     t.index ["record_id"], name: "index_agent_associations_on_record_id"
     t.index ["record_type"], name: "index_agent_associations_on_record_type"
   end
 
   create_table "agents", force: :cascade do |t|
-    t.string "uri", limit: 255
-    t.string "display_name", limit: 255
-    t.string "agent_type", limit: 255
-    t.text "api_response", limit: 4294967295
+    t.string "uri"
+    t.string "display_name"
+    t.string "agent_type"
+    t.text "api_response"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["agent_type"], name: "index_agents_on_agent_type"
     t.index ["uri"], name: "index_agents_on_uri"
   end
 
-  create_table "archival_objects", force: :cascade do |t|
-    t.string "uri", limit: 255, null: false
+  create_table "archival_objects", id: false, force: :cascade do |t|
+    t.integer "id", null: false
+    t.string "uri", null: false
     t.string "title", limit: 8704
     t.boolean "publish"
-    t.integer "parent_id", limit: 4
-    t.integer "resource_id", limit: 4
-    t.integer "repository_id", limit: 4, null: false
-    t.string "level", limit: 255
-    t.integer "position", limit: 4
-    t.text "api_response", limit: 4294967295
+    t.integer "parent_id"
+    t.integer "resource_id"
+    t.integer "repository_id", null: false
+    t.string "level"
+    t.integer "position"
+    t.text "api_response"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text "unit_data", limit: 4294967295
+    t.text "unit_data"
     t.boolean "has_children"
-    t.integer "load_position", limit: 4
-    t.string "component_id", limit: 255
+    t.integer "load_position"
+    t.string "component_id"
     t.index ["parent_id"], name: "index_archival_objects_on_parent_id"
     t.index ["repository_id"], name: "index_archival_objects_on_repository_id"
     t.index ["resource_id"], name: "index_archival_objects_on_resource_id"
@@ -64,36 +65,37 @@ ActiveRecord::Schema.define(version: 2018_09_18_141027) do
   create_table "aspace_imports", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "import_type", limit: 255
-    t.integer "resources_updated", limit: 4
+    t.string "import_type"
+    t.integer "resources_updated"
     t.integer "import_errors"
   end
 
   create_table "digital_object_associations", force: :cascade do |t|
-    t.integer "record_id", limit: 4
-    t.string "record_type", limit: 255
-    t.integer "digital_object_id", limit: 4
-    t.integer "position", limit: 4
+    t.integer "record_id"
+    t.string "record_type"
+    t.integer "digital_object_id"
+    t.integer "position"
     t.index ["digital_object_id"], name: "index_digital_object_associations_on_digital_object_id"
     t.index ["record_id"], name: "index_digital_object_associations_on_record_id"
     t.index ["record_type"], name: "index_digital_object_associations_on_record_type"
   end
 
   create_table "digital_object_volumes", force: :cascade do |t|
-    t.integer "digital_object_id", limit: 4, null: false
-    t.integer "position", limit: 4, null: false
+    t.integer "digital_object_id", null: false
+    t.integer "position", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "volume_id", limit: 4
+    t.integer "volume_id"
   end
 
-  create_table "digital_objects", force: :cascade do |t|
-    t.string "uri", limit: 255, null: false
-    t.integer "repository_id", limit: 4, null: false
+  create_table "digital_objects", id: false, force: :cascade do |t|
+    t.integer "id", null: false
+    t.string "uri", null: false
+    t.integer "repository_id", null: false
     t.string "title", limit: 8704
-    t.string "digital_object_id", limit: 255
+    t.string "digital_object_id"
     t.boolean "publish"
-    t.text "api_response", limit: 4294967295
+    t.text "api_response"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean "show_thumbnails"
@@ -101,49 +103,51 @@ ActiveRecord::Schema.define(version: 2018_09_18_141027) do
     t.index ["uri"], name: "index_digital_objects_on_uri"
   end
 
-  create_table "repositories", force: :cascade do |t|
-    t.string "uri", limit: 255, null: false
-    t.string "repo_code", limit: 255, null: false
-    t.string "org_code", limit: 255
-    t.string "name", limit: 255, null: false
-    t.text "api_response", limit: 4294967295
+  create_table "repositories", id: false, force: :cascade do |t|
+    t.integer "id", null: false
+    t.string "uri", null: false
+    t.string "repo_code", null: false
+    t.string "org_code"
+    t.string "name", null: false
+    t.text "api_response"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["repo_code"], name: "index_repositories_on_repo_code"
   end
 
-  create_table "resources", force: :cascade do |t|
-    t.string "uri", limit: 255, null: false
-    t.integer "repository_id", limit: 4, null: false
+  create_table "resources", id: false, force: :cascade do |t|
+    t.integer "id", null: false
+    t.string "uri", null: false
+    t.integer "repository_id", null: false
     t.string "title", limit: 8704
     t.boolean "publish"
-    t.text "api_response", limit: 4294967295
+    t.text "api_response"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text "unit_data", limit: 4294967295
+    t.text "unit_data"
     t.boolean "has_children"
-    t.text "structure", limit: 4294967295
-    t.integer "total_components", limit: 4
-    t.integer "total_top_components", limit: 4
-    t.string "eadid", limit: 255
+    t.text "structure"
+    t.integer "total_components"
+    t.integer "total_top_components"
+    t.string "eadid"
     t.index ["eadid"], name: "index_resources_on_eadid"
     t.index ["repository_id"], name: "index_resources_on_repository_id"
     t.index ["uri"], name: "index_resources_on_uri"
   end
 
   create_table "search_indices", force: :cascade do |t|
-    t.string "index_type", limit: 255
-    t.integer "records_updated", limit: 4
+    t.string "index_type"
+    t.integer "records_updated"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "subject_associations", force: :cascade do |t|
-    t.integer "record_id", limit: 4
-    t.string "record_type", limit: 255
-    t.integer "subject_id", limit: 4
-    t.string "function", limit: 255
-    t.integer "position", limit: 4
+    t.integer "record_id"
+    t.string "record_type"
+    t.integer "subject_id"
+    t.string "function"
+    t.integer "position"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["record_id"], name: "index_subject_associations_on_record_id"
@@ -151,13 +155,14 @@ ActiveRecord::Schema.define(version: 2018_09_18_141027) do
     t.index ["subject_id"], name: "index_subject_associations_on_subject_id"
   end
 
-  create_table "subjects", force: :cascade do |t|
-    t.string "uri", limit: 255
-    t.string "subject", limit: 255
-    t.string "subject_root", limit: 255
-    t.string "subject_type", limit: 255
-    t.string "subject_source_uri", limit: 255
-    t.text "api_response", limit: 4294967295
+  create_table "subjects", id: false, force: :cascade do |t|
+    t.integer "id", null: false
+    t.string "uri"
+    t.string "subject"
+    t.string "subject_root"
+    t.string "subject_type"
+    t.string "subject_source_uri"
+    t.text "api_response"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["subject_type"], name: "index_subjects_on_subject_type"
