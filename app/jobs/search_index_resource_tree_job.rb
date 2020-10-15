92787b8abb8191a04a8f7a5e8cd08789a133e636
Trevor Ryan Thornton <trthorn2@ncsu.edu>
2020-10-15 16:13:00 -0400

    serach index tree service

--- app/jobs/search_index_resource_tree_job.rb
+++ app/jobs/search_index_resource_tree_job.rb
@@ -2,7 +2,7 @@ class SearchIndexResourceTreeJob < ApplicationJob
   queue_as :index
 
   def perform(resource_id)
-    SearchIndexResourceTreeService.call(resource_id)
+    SearchIndexResourceTreeService.call(resource_id: resource_id.to_i)
   end
 
 end
