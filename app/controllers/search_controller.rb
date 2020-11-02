10767abfc8598ddb93fe9064bb305000754bfc8e
Trevor Ryan Thornton <trthorn2@ncsu.edu>
2020-11-02 12:59:13 -0500

    fix string sanitizer

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -45,7 +45,7 @@ class SearchController < ApplicationController
     if @params[:filters]['inclusive_years']
       ranges = []
       @params[:filters]['inclusive_years'].each do |range|
-        dates = range.split('-').map { |x| sanitize_integer(x).to_i }
+        dates = range.split('-').map { |x| SolrSanitizer.sanitize_integer(x).to_i }
         ranges << "[#{dates[0]} TO #{dates[1]}]"
       end
       @params[:filters][:inclusive_years] = ranges
