601334b47ec725f1e123c604fc4c76793ab53c1c
Trevor Thornton <trthorn2@ncsu.edu>
2021-04-14 15:26:09 -0400

    make delete resources service reporting only

--- app/services/purge_deleted_resources.rb
+++ app/services/purge_deleted_resources.rb
@@ -1,16 +1,17 @@
 # This service has been placed in "Reporting mode"!
-@@reporting_mode = true
+
 
 class PurgeDeletedResources
 
   include GeneralUtilities
 
-  def self.call
-    object = new
+  def self.call(options={})
+    object = new(options={})
     object.call
   end
 
-  def initialize
+  def initialize(options={})
+    @options = options
   end
 
   def call
@@ -22,8 +23,19 @@ class PurgeDeletedResources
 
 
   def execute
+
+    @reporting_mode = true
+
     log_info "PurgeDeletedResources called"
     log_info "Purging resources that have been deleted in ArchivesSpace (or with finding aid status other than 'completed')..."
+
+    if @options[:delete_resources]
+      log_info "Called with :delete_resources - @reporting_mode disabled. RESOURCES WILL BE DESTROYED!"
+      @reporting_mode = false
+    else
+      log_info "@reporting_mode enabled - no records will actually be deleted. It's cool."
+    end
+
     query_params = { 'fq' => ["publish:true", "finding_aid_status:(Completed OR completed)"] }
     @resources_deleted = 0
     @resource_count = Resource.count
@@ -32,7 +44,7 @@ class PurgeDeletedResources
     start_id = 0
 
 
-    # *** REPORTING MODE ***
+    # *** FOR REPORTING MODE ***
     @missing_resources = []
 
     while i < @resource_count
@@ -63,15 +75,19 @@ class PurgeDeletedResources
         batch.each do |fields|
           id, uri = fields
           if !solr_doc_exists?(uri,query_params)
-            log_info "#{uri} no longer exists - deleting..."
+            
 
-            # *** REPORTING MODE - REMOVED THE CALL TO DELETE *** 
-            ## DELETE IT!
-            ## r = Resource.find_by_uri(uri)
-            ## r.destroy
+            # *** REPORTING MODE - REMOVES THE CALL TO DELETE *** 
+            if !@reporting_mode
+              log_info "#{uri} no longer exists - deleting..."
+              ## DELETE IT!
+              r = Resource.find_by_uri(uri)
+              r.destroy
+            else
+              log_info "Not found in ArchivesSpace index: #{uri}"
+            end
 
             # *** REPORTING MODE ***
-            log_info "Not found in ArchivesSpace index: #{uri}"
             @missing_resources << uri
 
             @resources_deleted += 1
@@ -87,16 +103,17 @@ class PurgeDeletedResources
       end
     end
 
+    # *** REPORTING MODE - REMOVES THE CALL TO CREATE AspaceImport record *** 
+    if !@reporting_mode
+      if @resources_deleted > 0
+        AspaceImport.create(import_type: 'purge_deleted', resources_updated: @resources_deleted)
+      end
+    end
 
-    # *** REPORTING MODE - REMOVED THE CALL TO CREATE AspaceImport record *** 
-    # if @resources_deleted > 0
-    #   AspaceImport.create(import_type: 'purge_deleted', resources_updated: @resources_deleted)
-    # end
-
-
-    # *** REPORTING MODE - REMOVED THE CALL TO DELETE ORPHANED RECORDS ***
-    # ArchivalObject.delete_orphans
-
+    # *** REPORTING MODE - REMOVES THE CALL TO DELETE ORPHANED RECORDS ***
+    if !@reporting_mode
+      ArchivalObject.delete_orphans
+    end
 
     log_info "AspaceImport.purge_deleted complete"
     log_info "Missing resources:"
@@ -106,10 +123,7 @@ class PurgeDeletedResources
 
   def solr_doc_exists?(uri,params)
     query = "id:#{escape_uri(uri)}"
-
-    # response = execute_solr_query(query,params)
     response = ExecuteAspaceSolrQuery.call(query: query, params: params)
-
     return (response['response']['numFound'] == 0) ? false : true
   end
 
@@ -117,6 +131,5 @@ class PurgeDeletedResources
   def escape_uri(uri)
     uri.gsub(/\//,'\/')
   end
-
 
 end
