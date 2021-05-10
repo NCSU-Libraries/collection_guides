cf5f49eed1eb18d4f3f376c3285ab4b5eed97185
Trevor Thornton <trthorn2@ncsu.edu>
2021-05-10 15:58:40 -0400

    add middleware to rescue from bad query encoding

--- app/middleware/handle_bad_encoding_middleware.rb
+++ app/middleware/handle_bad_encoding_middleware.rb
@@ -6,10 +6,10 @@ class HandleBadEncodingMiddleware
   def call(env)
     begin
       Rack::Utils.parse_nested_query(env['QUERY_STRING'].to_s)
-    rescue Rack::Utils::InvalidParameterError
+    rescue Rack::Utils::InvalidParameterError, Rack::QueryParser::InvalidParameterError, ActionController::BadRequest
       env['QUERY_STRING'] = ''
     end
-    
+
     @app.call(env)
   end
 end
