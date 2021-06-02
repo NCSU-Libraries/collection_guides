3c4f6c6336835bebdcb70e901d1c8792c3e06d76
Trevor Thornton <trthorn2@ncsu.edu>
2021-06-02 11:44:24 -0400

    update bundle for ruby 2.7.2

---
+++ public/assets/ncsul_web/ie8-ff3193e823caee70cd0ce8619aea5408579ca11fcb03fcdf1cb0446f4fd6f151.js
@@ -0,0 +1,16 @@
+window.onload = function (){
+    // add browser warning
+    thebody = document.getElementsByTagName('body')[0];
+    str = "<div id='browser_warning'><h4>This site may not work well on Internet Explorer versions 8 or lower</h4></div>";
+    thediv = document.createElement('div');
+    thediv.innerHTML = str;
+    thebody.insertBefore(thediv, thebody.firstChild);
+
+    //placeholder polyfill
+    var input = document.getElementsByTagName('input');
+    var textarea = document.getElementsByTagName('textarea');
+    $(input).placeholder();
+    $(textarea).placeholder();
+}
+
+;
