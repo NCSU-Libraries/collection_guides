9e4cc0b13223500ae6acadb0b97f9b7e4ddd5609
Trevor Thornton <trthorn2@ncsu.edu>
2021-06-04 15:46:39 -0400

    try something with asset manifest to fix deploy issue

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,3 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link ../javascripts/application.js
+//= link ../stylesheets/application.css
